bb9c609d3d216b83658ae057129d3505bd6726a2
lucasaf04 <39613949+lucasaf04@users.noreply.github.com>
2026-03-03 15:52:01 +0100

    Cambios ejercicio 7.

--- hipster-core/src/main/java/es/usc/citius/hipster/algorithm/localsearch/AnnealingSearch.java
+++ hipster-core/src/main/java/es/usc/citius/hipster/algorithm/localsearch/AnnealingSearch.java
@@ -66,6 +66,7 @@ public class AnnealingSearch<A, S, N extends HeuristicNode<A, S, Double, N>> ext
 	private SuccessorFinder<A, S, N> successorFinder;
 	// expander to find all the successors of a given node.
 	private NodeExpander<A, S, N> nodeExpander;
+	private Random random = new Random();
 
 	public AnnealingSearch(N initialNode, NodeExpander<A, S, N> nodeExpander, Double alpha, Double minTemp,
 			AcceptanceProbability acceptanceProbability, SuccessorFinder<A, S, N> successorFinder) {
@@ -116,8 +117,7 @@ public class AnnealingSearch<A, S, N extends HeuristicNode<A, S, Double, N>> ext
 					for (N successor : nodeExpander.expand(node)) {
 						successors.add(successor);
 					}
-					Random randIndGen = new Random();
-					return successors.get(Math.abs(randIndGen.nextInt()) % successors.size());
+					return successors.get(Math.abs(this.random.nextInt()) % successors.size());
 				}
 			};
 		}
